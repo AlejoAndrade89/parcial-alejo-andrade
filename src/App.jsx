694c08ff7e10e5eb5e0648b7e6bf4f3c90e11c3b
DAGA <andrade.can89@gmail.com>
2023-03-07 21:30:44 -0700

    subiend plantilla parcial

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import { Card } from "./Card/Card";
+
+function App() {
+  return (
+    <div>
+      <h1>Carga de estudiantes</h1>
+      <form action=""></form>
+      <Card />
+    </div>
+  );
+}
+
+export default App;
